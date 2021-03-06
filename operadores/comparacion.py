b5be58ba4cecece9c4fc837002585260a46dccc8
Cesar Perez <teramita45@gmail.com>
2021-03-06 17:14:35 -0500

    avanzodando dia 1

---
+++ operadores/comparacion.py
@@ -0,0 +1,6 @@
+a = 3
+b = 2
+
+resultado = a == b
+print(resultado)
+
---
+++ operadores/operadores-aritmetico.py
@@ -0,0 +1,18 @@
+a = int(input("dame un mumero"))
+b = int(input("dame otro numero"))
+suma = a+b
+print("la suma es : ",suma)
+
+multi= a * b
+
+print(multi)
+
+division = a/b
+print(division) 
+
+modulo = a % b
+print("modulo: " , modulo)
+
+exponente = a **b
+print("exponente: ", exponente)
+
